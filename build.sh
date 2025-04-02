b14e23ca9b8bcee1a063232cf430343bbc482681
Ivan Vnucec <vnucec.ivan@gmail.com>
2025-04-02 20:00:36 +0200

    add defconfig flag and change build path

--- build.sh
+++ build.sh
@@ -3,11 +3,12 @@
 set -x -e
 
 DEVICE_TREE=stm32mp157-somic
-BUILD_DIR=build-$DEVICE_TREE
+DEFCONFIG=stm32mp157_somic_defconfig
+BUILD_DIR=build
 
 rm -rf $BUILD_DIR
 make mrproper
 export KBUILD_OUTPUT=$BUILD_DIR
-make stm32mp157_somic_defconfig
+make $DEFCONFIG
 make -j8 CROSS_COMPILE=arm-linux-gnueabihf- DEVICE_TREE=$DEVICE_TREE all
 unset KBUILD_OUTPUT
